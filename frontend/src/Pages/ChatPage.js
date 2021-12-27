22a13f63c0ed8fa8d30ddcce844aa0c374d7c589
starLightaa <iip2205@mail.ru>
2021-12-27 09:43:54 +0300

    UI for Users -> Chat crud operations
    Кнопки создать/удалить чат
    Добавить в чат, переименовать и т.д

--- frontend/src/Pages/ChatPage.js
+++ frontend/src/Pages/ChatPage.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Box } from "@chakra-ui/layout";
 import SideDrawer from "../components/miscellaneous/SideDrawer";
 import Chatbox from "../components/Chatbox";
@@ -6,12 +7,15 @@ import { ChatState } from "../Context/ChatProvider";
 
 const ChatPage = () => {
   const { user } = ChatState();
+  const [fetchAgain, setFetchAgain] = useState(false);
   return (
     <div style={{ width: "100%" }}>
       {user && <SideDrawer />}
       <Box d="flex" justifyContent="space-between" w="100%" h="91.5vh" p="10px">
-        {user && <MyChats />}
-        {user && <Chatbox />}
+        {user && <MyChats fetchAgain={fetchAgain} />}
+        {user && (
+          <Chatbox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
+        )}
       </Box>
     </div>
   );
