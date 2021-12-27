001ec08523d79b490c02d626753b2e754f9986f7
starLightaa <iip2205@mail.ru>
2021-12-27 10:29:41 +0300

    Message APIs

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const { chats } = require("./data/data");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 const app = express();
@@ -18,6 +19,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
 
 // Error Handling middlewares
 app.use(notFound);
